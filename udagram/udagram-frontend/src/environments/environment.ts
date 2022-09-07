2014adcb7561ee0e37d7b8548b3ec7bd246b699d
Nico Gombert <nico.gombert@outlook.de>
2022-09-07 12:29:46 +0200

    lint

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -7,7 +7,6 @@ export const environment = {
   appName: 'Udagram',
   // apiHost: 'http://localhost:8080/api/v0'
   apiHost: 'http://udagram-api-dev.eba-5mpcxihk.us-east-1.elasticbeanstalk.com/api/v0'
-  
 };
 
 /*
